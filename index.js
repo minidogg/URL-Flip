e469d7441d9daa0a3f91a1d80137b06cca2389e0
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-12-15 16:43:30 -0600

    nav

--- index.js
+++ index.js
@@ -9,20 +9,28 @@ const pages = {
   redirect: "",
 };
 
+
 function UpdateDyanmicPages() {
+  let navHTML = fs.readFileSync(path.resolve("./static/nav.html"), "utf-8")
   pages.index = fs
     .readFileSync(path.resolve("./pages/index.html"), "utf-8")
+    .replace("<nav />", navHTML)
     .split("LAST_LINKS");
 
   pages.error = fs
     .readFileSync(path.resolve("./pages/error.html"), "utf-8")
+    .replace("<nav />", navHTML)
     .split("ERROR"); // We split it at ERROR so then we can do errorHTML.join(errorMsg)
   pages.share = fs
     .readFileSync(path.resolve("./pages/share.html"), "utf-8")
+    .replace("<nav />", navHTML)
     .split("SHARE_LINK");
   pages.redirect = fs
     .readFileSync(path.resolve("./pages/redirect.html"), "utf-8")
+    .replace("<nav />", navHTML)
     .split("REDIRECT_LINK");
+
+
 }
 UpdateDyanmicPages();
 let havePagesUpdated = false;
