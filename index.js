b5ac85a2c48986340a9165987afc7f72941b032c
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-12-15 17:05:34 -0600

    move pages stuff into its own file

--- index.js
+++ index.js
@@ -1,54 +1,13 @@
-// Imports
+// Built-In Imports
 import path from 'path';
 import fs from 'fs';
 
+// Node Module Imports
 import formidable from "express-formidable";
 import express from "express";
 
-// We don't need frameworks or templating engines where we are going.
-const pages = {
-  index: "",
-  error: "",
-  share: "",
-  redirect: "",
-};
-
-
-function UpdateDyanmicPages() {
-  let navHTML = fs.readFileSync(path.resolve("./static/nav.html"), "utf-8")
-  pages.index = fs
-    .readFileSync(path.resolve("./pages/index.html"), "utf-8")
-    .replace("<nav />", navHTML)
-    .split("LAST_LINKS");
-
-  pages.error = fs
-    .readFileSync(path.resolve("./pages/error.html"), "utf-8")
-    .replace("<nav />", navHTML)
-    .split("ERROR"); // We split it at ERROR so then we can do errorHTML.join(errorMsg)
-  pages.share = fs
-    .readFileSync(path.resolve("./pages/share.html"), "utf-8")
-    .replace("<nav />", navHTML)
-    .split("SHARE_LINK");
-  pages.redirect = fs
-    .readFileSync(path.resolve("./pages/redirect.html"), "utf-8")
-    .replace("<nav />", navHTML)
-    .split("REDIRECT_LINK");
-
-
-}
-UpdateDyanmicPages();
-let havePagesUpdated = false;
-fs.watch("./pages", { recursive: true }, (eventName, fileName) => {
-  console.log("Page updated: " + fileName);
-  havePagesUpdated = true;
-});
-setInterval(() => {
-  if (havePagesUpdated == true) {
-    havePagesUpdated = false;
-    UpdateDyanmicPages();
-    console.log("Updated pages");
-  }
-}, 1000);
+// Local Imports
+import { pages } from './pages.js';
 
 const app = express();
 const port = 3000;
---
+++ pages.js
@@ -0,0 +1,44 @@
+// We don't need frameworks or templating engines where we are going.
+export const pages = {
+  index: "",
+  error: "",
+  share: "",
+  redirect: "",
+};
+
+
+function UpdateDyanmicPages() {
+  let navHTML = fs.readFileSync(path.resolve("./static/nav.html"), "utf-8")
+  pages.index = fs
+    .readFileSync(path.resolve("./pages/index.html"), "utf-8")
+    .replace("<nav />", navHTML)
+    .split("LAST_LINKS");
+
+  pages.error = fs
+    .readFileSync(path.resolve("./pages/error.html"), "utf-8")
+    .replace("<nav />", navHTML)
+    .split("ERROR"); // We split it at ERROR so then we can do errorHTML.join(errorMsg)
+  pages.share = fs
+    .readFileSync(path.resolve("./pages/share.html"), "utf-8")
+    .replace("<nav />", navHTML)
+    .split("SHARE_LINK");
+  pages.redirect = fs
+    .readFileSync(path.resolve("./pages/redirect.html"), "utf-8")
+    .replace("<nav />", navHTML)
+    .split("REDIRECT_LINK");
+
+
+}
+UpdateDyanmicPages();
+let havePagesUpdated = false;
+fs.watch("./pages", { recursive: true }, (eventName, fileName) => {
+  console.log("Page updated: " + fileName);
+  havePagesUpdated = true;
+});
+setInterval(() => {
+  if (havePagesUpdated == true) {
+    havePagesUpdated = false;
+    UpdateDyanmicPages();
+    console.log("Updated pages");
+  }
+}, 1000);
